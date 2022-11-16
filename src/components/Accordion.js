0afe96ca18acef12a2f9e652845929ba9d5cc834
Fabricio Palavecino <fpalavecino@altimetrik.com>
2022-11-16 20:35:46 -0300

    first commit

---
+++ src/components/Accordion.js
@@ -0,0 +1,31 @@
+import React,{Fragment, useState} from "react";
+
+const Accordion = ({ items }) => {
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index)  => {
+    setActiveIndex(index)
+  };
+
+  const renderedItems = items.map((item, index) => {
+
+    const active = index === activeIndex ? 'active' : '';
+
+    return <Fragment key={item.title}>
+      <div className={`${active} title`} onClick={() => onTitleClick(index)}>
+        <i className="dropdown icon"></i>
+        {item.title}
+      </div>
+      <div className={`${active} content`}>
+        <p>{item.content}</p>
+      </div>
+    </Fragment>
+  })
+  return(
+    <div className="ui styled accordion">
+      {renderedItems}
+      </div>
+  )
+}
+export default Accordion;
